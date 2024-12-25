2c25f4d9db57d7b2ec1eed2341e34ec32e59b4fd
Juno Crisitan <juno.lp@hotmail.com>
2024-12-25 17:40:07 -0300

    atualizado

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import theme from './theme.ts'
+import theme from './theme.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
